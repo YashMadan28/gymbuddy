2c9c093239c779366ae445087b4b79b0db079ce6
lmmelgoza <luismelgoza101@gmail.com>
2025-02-26 15:49:53 -0800

    visual updates to footer and header buttons

--- GymBuddy/src/App.jsx
+++ GymBuddy/src/App.jsx
@@ -1,24 +1,23 @@
-import { useState } from 'react'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import AppBar from './AppBar';
 import AppLayout from './AppLayout';
 import Profile from './Profile';
 import MainPage from './MainPage';
-import { Link } from "react-router-dom";
 
 
 function App() {
 
   return (
     <Router>
-      <AppBar /> {AppBar}
       <Routes>
         <Route element = {<AppLayout />}>
           <Route path = "/" element = { <MainPage />} />
           <Route path = "/profile" element = {<Profile />} />
+          {/*<Route path = "/login" element = {<Login />} />*/}
+          {/*<Route path = "/messages" element = {<Messages />} />*/}
+          {/*<Route path = "/stats" element = {<Stats />} />*/}
+          {/*<Route path = "/Schedule" element = {<Schedule />} />*/}
+          {/*<Route path = "/workout_library" element = {<Workout_Library />} />*/}
         </Route>
       </Routes>
     </Router>
--- GymBuddy/src/AppBar.jsx
+++ GymBuddy/src/AppBar.jsx
@@ -2,13 +2,9 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
-import IconButton from '@mui/material/IconButton';
-import Drawer from '@mui/material/Drawer';
-import MenuIcon from '@mui/icons-material/Menu';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
+
 
 const Header = () => {
   return (
@@ -20,12 +16,13 @@ const Header = () => {
         height: '90px',
         boxShadow: "none",
         }}
-    >
+    > 
       <Toolbar sx = {{ display: 'flex', justifyContent: 'center', paddingTop: '13px'}}>
+        <Link 
+          to = "/profile"
+          style = {{ textDecoration: 'none' }}
+          >
           <Button
-            component = {Link}
-            to = "/profile"
-            color = "inherit"
             sx = {{
               padding: '10px 20px',
               border: 'none',
@@ -35,14 +32,17 @@ const Header = () => {
               letterSpacing: '4px',
               fontWeight: 700,
               textTransform: 'uppercase',
-              transition: 'box-shadow 0.5s',
-              background: 'rgb(0, 0, 0)',
+              transition: 'box-shadow 0.5s, background-color 0.5s',
+              backgroundColor: 'rgb(0, 0, 0)',
               boxShadow: '0 0 25px rgb(0, 0, 0)',
+              textDecoration: 'none',
               '&:hover': {
-                boxShadow: '0 0 5px rgb(0, 140, 255), 0 0 0px rgb(0, 255, 26), 0 0 50px rgb(0,140,255), 0 0 100px rgb(0,140,255)'
+                boxShadow: '0 0 5px rgb(0, 140, 255), 0 0 0px rgb(0, 140, 255), 0 0 50px rgb(0,140,255), 0 0 100px rgb(0,140,255)',
+                background: 'rgb(0, 140, 255)',
               },
             }}
           >Profile</Button>
+          </Link>
           <Typography 
             variant = "h6" 
             component = "div" 
@@ -68,11 +68,12 @@ const Header = () => {
               letterSpacing: '4px',
               fontWeight: 700,
               textTransform: 'uppercase',
-              transition: 'box-shadow 0.5s',
-              background: 'rgb(0, 0, 0)',
+              transition: 'box-shadow 0.5s, background-color 0.5s',
+              backgroundColor: 'rgb(0, 0, 0)',
               boxShadow: '0 0 25px rgb(0, 0, 0)',
               '&:hover': {
-                boxShadow: '0 0 5px rgb(0, 140, 255), 00 00px rgb(0, 255, 26), 0 0 50px rgb(0,140,255), 0 0 100px rgb(0,140,255)'
+                boxShadow: '0 0 5px rgb(0, 140, 255), 00 00px rgb(0, 255, 26), 0 0 50px rgb(0,140,255), 0 0 100px rgb(0,140,255)',
+                background: 'rgb(0, 140, 255)',
               },
             }}
           >Log in</Button>
