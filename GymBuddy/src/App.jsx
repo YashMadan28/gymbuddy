867542c82937c09b35c9c785cd5a64af367d4009
CharlieThao <charliethao347@hotmail.com>
2025-03-03 12:31:05 -0800

    Added option to upload profile picture. Closes #2

--- GymBuddy/src/App.jsx
+++ GymBuddy/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css'
 import AppLayout from './AppLayout';
 import Profile from './Profile';
+import EditProfile from './EditProfile';
 import MainPage from './MainPage';
 
 
@@ -13,6 +14,7 @@ function App() {
         <Route element = {<AppLayout />}>
           <Route path = "/" element = { <MainPage />} />
           <Route path = "/profile" element = {<Profile />} />
+          <Route path= "/profile/edit" element={<EditProfile />} />
           {/*<Route path = "/login" element = {<Login />} />*/}
           {/*<Route path = "/messages" element = {<Messages />} />*/}
           {/*<Route path = "/stats" element = {<Stats />} />*/}
