66c66f78227f6ac82642de52a1c304116896d14d
ymadan <ymadan@csuchico.edu>
2025-03-03 12:33:35 -0800

    home page improvement

--- GymBuddy/src/main.jsx
+++ GymBuddy/src/main.jsx
@@ -1,4 +1,4 @@
-import { StrictMode } from 'react'
+/*import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
@@ -7,4 +7,25 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
-)
+)*/
+import React from 'react';
+import Footer from './Footer';
+import Typing from 'react-typing-animation';
+import './MainPage.css'; // Import the CSS file for styling
+
+const MainPage = () => {
+  return (
+    <div className="main-container">
+      <div className="text-container">
+        <Typing speed={100}>
+          <h1 className="typing-text">Ready to meet your workout partner?</h1>
+        </Typing>
+      </div>
+      <div className="image-container">
+        <img src="/src/assests/home page.jpg" alt="Workout partners" />
+      </div>
+    </div>
+  );
+};
+
+export default MainPage;
