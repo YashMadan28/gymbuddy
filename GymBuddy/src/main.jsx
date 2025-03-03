5a5dc05919e0f5534b077cbb37550b1570a9200b
ymadan <ymadan@csuchico.edu>
2025-03-03 12:39:50 -0800

    removed the edits

--- GymBuddy/src/main.jsx
+++ GymBuddy/src/main.jsx
@@ -8,20 +8,3 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )*/
-import React from 'react';
-import './index.css';
-
-const MainPage = () => {
-  return (
-    <div className="main-container">
-      <div className="text-container">
-        <h1 className="static-text">Ready to meet your workout partner?</h1>
-      </div>
-      <div className="image-container">
-        <img src="/src/assests.home page.jpg" alt="Workout partners" />
-      </div>
-    </div>
-  );
-};
-
-export default MainPage
