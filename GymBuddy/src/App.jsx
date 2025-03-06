3dd10a8bf53bad89234b61796082e713cc1a6a0d
ymadan <ymadan@csuchico.edu>
2025-03-06 14:00:44 -0800

    CHnaging app.jsx file

--- GymBuddy/src/App.jsx
+++ GymBuddy/src/App.jsx
@@ -1,32 +1,4 @@
-/*import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css'
-import AppLayout from './AppLayout';
-import Profile from './Profile';
-import EditProfile from './EditProfile';
-import MainPage from './MainPage';
-import { AuthProvider } from './context/AuthContext';
 
-function App() {
-
-  return (
-    <Router>
-      <Routes>
-        <Route element = {<AppLayout />}>
-          <Route path = "/" element = { <MainPage />} />
-          <Route path = "/profile" element = {<Profile />} />
-          <Route path= "/profile/edit" element={<EditProfile />} />
-          {/*<Route path = "/login" element = {<Login />} />*/}
-          {/*<Route path = "/messages" element = {<Messages />} />*/}
-          {/*<Route path = "/stats" element = {<Stats />} />*/}
-          {/*<Route path = "/Schedule" element = {<Schedule />} />*/}
-          {/*<Route path = "/workout_library" element = {<Workout_Library />} />*/}
-       /* </Route>
-      </Routes>
-    </Router>
-  );
-}
-
-export default App*/
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';  // Import the AuthProvider
