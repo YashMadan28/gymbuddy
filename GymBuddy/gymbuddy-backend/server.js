cdf33a8016194cc78c14c303aad155e9b074e87f
CharlieThao <charliethao347@hotmail.com>
2025-04-09 23:30:26 -0700

    Save changes before pulling upstream

--- GymBuddy/gymbuddy-backend/server.js
+++ GymBuddy/gymbuddy-backend/server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, retryWrites: true, w: 'majority', ssl: true, })
