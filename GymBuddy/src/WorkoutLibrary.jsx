8679c19284f6aafa6f266c07673661cf1ebe9a57
lmmelgoza <luismelgoza101@gmail.com>
2025-04-28 13:03:56 -0700

    Fixed some code smells

--- GymBuddy/src/WorkoutLibrary.jsx
+++ GymBuddy/src/WorkoutLibrary.jsx
@@ -3,100 +3,13 @@ import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
 import { onAuthStateChanged } from "firebase/auth";
+import { workoutData } from './workoutData';
 import "./animations.css";
 
 <Link to="/workout_library">
   <button>Workout Library</button>
 </Link>;
 
-const workoutData = {
-  splits: [
-    {
-      name: "Upper/Lower Split",
-      days: [
-        {
-          dayName: "Day 1: Upper Body",
-          focus: "Chest, Shoulders, Triceps",
-          exercises: ["Bench Press", "Overhead Press", "Tricep Pushdowns"],
-        },
-        {
-          dayName: "Day 2: Lower Body",
-          focus: "Legs, Glutes",
-          exercises: ["Squats", "Deadlifts", "Leg Press"],
-        },
-      ],
-    },
-    {
-      name: "Push/Pull/Legs Split",
-      days: [
-        {
-          dayName: "Push Day",
-          focus: "Chest, Shoulders, Triceps",
-          exercises: [
-            "Bench Press",
-            "Dumbell Fly",
-            "Incline Bench Press",
-            "Shoulder Press",
-            "Tricep Extensions",
-          ],
-        },
-        {
-          dayName: "Pull Day",
-          focus: "Chest Shoulders, Triceps",
-          exercises: [
-            "Incline Dumbbell Press",
-            "Lat Pulldown",
-            "Cable Crossovers",
-            "Bicep Curls",
-            "Deadlift",
-          ],
-        },
-        {
-          dayName: "Leg Day",
-          focus: "Quads, Hamstrings, Glutes",
-          exercises: ["Leg Extentions", "Romanian Deadlifts", "Calf Raises"],
-        },
-      ],
-    },
-  ],
-  muscleGroups: [
-    {
-      groupName: "Chest",
-      exercises: [
-        "Push-ups",
-        "Cable Flyes",
-        "Dumbbell Press",
-        "Decline Bench Press",
-      ],
-    },
-    {
-      groupName: "Back",
-      exercises: ["Pull-ups", "Lat Pulldowns", "Face Pulls", "Deadlifts"],
-    },
-    {
-      groupName: "Legs",
-      exercises: ["Leg Extensions", "Leg Press", "Walking Lunges", "Squats"],
-    },
-    {
-      groupName: "Shoulders",
-      exercises: [
-        "Dumbell Lateral Raise",
-        "Dumbell Shoulder Press",
-        "Face Pull",
-        "Rear Delt Flys",
-      ],
-    },
-    {
-      groupName: "Arms",
-      exercises: [
-        "Hammer Curl",
-        "Cable Curl",
-        "Tricep Pulldown",
-        "Skull Crushers",
-      ],
-    },
-  ],
-};
 
 function WorkoutLibrary() {
   const [activeSplit, setActiveSplit] = useState(null);
